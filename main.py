841b62e4cf2f53d4fda6553527b3c7a0418596ec
moses <ghost@moses.com>
2024-08-05 11:48:03 +0100

    feat: added greeting route
    - Ensured endpoint for greeting available

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World2"}
+
+
+@app.get("/greetings")
+def greetings():
+    return "Greeting smurfs"
 
 
 @app.get("/items/{item_id}")
