cc1d2fec84d20a2d9bb0b387f017cce8c10e6866
moses <ghost@moses.com>
2024-08-05 10:08:36 +0100

    fix: saved the commit

--- update_version.py
+++ update_version.py
@@ -9,6 +9,11 @@ def update_version_file(version):
     with open('VERSION', 'w') as f:
         f.write(version)
 
+def stage_and_amend():
+    subprocess.run(['git', 'add', 'VERSION'])
+    subprocess.run(['git', 'commit', '--amend', '--no-edit'])
+
 if __name__ == "__main__":
     latest_version = get_latest_version()
     update_version_file(latest_version)
+    stage_and_amend()
